a7781a7dc5ce63b626c508ba9c287a3831c104d2
王康 <13523912132@163.com>
2017-05-23 17:09:17 +0800

    Update urls.py

--- premier_league/urls.py
+++ premier_league/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
-    url(r'^premier/$',views.premier),
-    url(r'^premier/article/(?P<article_id>[0-9]+)$',views.article_page,name='article_page'),
+    url(r'^$',views.premier),
+    url(r'^/article/(?P<article_id>[0-9]+)$',views.article_page,name='article_page'),
     url(r'^edit/$',views.edit_page,name='edit_page'),
     url(r'^edit/action$',views.edit_action,name='edit_action'),
-    url(r'^premier/prediction$',views.prediction,name='prediction'),
+    url(r'^prediction$',views.prediction,name='prediction'),
 ]
 
 
